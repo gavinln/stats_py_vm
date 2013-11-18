f21ac4947ca91f587b707f49531975bf03686bb2
Gavin Noronha <gavinln@hotmail.com>
2013-11-17 19:50:42 -0800

    Added a bat file to run notebook on Windows

---
+++ vm/ipython_notebook.bat
@@ -0,0 +1,10 @@
+REM start the ipython notebook server
+
+
+
+set ROOT_DIR=%~dp0..
+set PYTHONPATH=%ROOT_DIR%\python
+;%PYTHONPATH%
+
+ipython notebook --port=8888 --ip=0.0.0.0 --no-browser --pylab=inline --notebook-dir=%ROOT_DIR%\notebooks --profile sympy
+
