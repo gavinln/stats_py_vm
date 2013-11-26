72bc1b180111719df6b81d16a112b368770dd250
Gavin Noronha <gavinln@hotmail.com>
2013-11-26 04:48:34 -0800

    improved notebook examples

--- vm/ipython_notebook.sh
+++ vm/ipython_notebook.sh
@@ -2,5 +2,11 @@
 
 ROOT_DIR=/vagrant
 export PYTHONPATH=$ROOT_DIR/python
-ipython notebook --port=8888 --ip=0.0.0.0 --no-browser --pylab=inline --notebook-dir=$ROOT_DIR/notebooks --profile sympy
+
+IPYTHON_CMD="ipython notebook --port=8888 --ip=0.0.0.0 --no-browser --pylab=inline"
+
+BASIC_NOTEBOOKS=$ROOT_DIR/notebooks
+VINCENT_NOTEBOOKS=$ROOT_DIR/vincent/examples
+
+$IPYTHON_CMD --notebook-dir=$BASIC_NOTEBOOKS --profile sympy
 
