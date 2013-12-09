08d37cae9560cc3763181e8dc0a667c8917328ba
Gavin Noronha <gavinln@hotmail.com>
2013-12-08 22:45:13 -0800

    Updated with images

--- puppet/manifests/classes/python.pp
+++ puppet/manifests/classes/python.pp
@@ -22,6 +22,11 @@ class python {
 #                command => "$HOME_DIR/unix/bin/pip install -r $PROJ_DIR/puppet/requirements/compiled.txt",
 #                require => Exec['virtualenv-create']
 #            }
+            package { 'untangle':
+                ensure => installed,
+                provider => pip,
+                require => Package['virtualenv']
+            }
             package { ['nose', 'tornado', 'jinja2']:
                 ensure => installed,
                 provider => pip,
