9b4f9f8c73bc975b9044d0d369d4a60e5b1de6fc
Gavin Noronha <gavinln@hotmail.com>
2014-05-26 23:04:59 -0700

    Added example notebook

--- vm/make_slides.sh
+++ vm/make_slides.sh
@@ -7,4 +7,5 @@ IPYTHON_CMD="ipython nbconvert"
 BASIC_NOTEBOOKS=$ROOT_DIR/notebooks
 
 #$IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_database.ipynb --to slides
-$IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_pyodbc.ipynb --to slides
+#$IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_pyodbc.ipynb --to slides
+$IPYTHON_CMD $BASIC_NOTEBOOKS/Responsive_design.ipynb --to slides
