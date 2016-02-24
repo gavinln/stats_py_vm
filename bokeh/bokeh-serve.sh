6ac36347371911cec8205f68050c6c19c0f5a66e
Gavin Noronha <gavinln@hotmail.com>
2016-02-23 18:32:42 -0800

    added instructions on how to allow access to host by ip

--- bokeh/bokeh-serve.sh
+++ bokeh/bokeh-serve.sh
@@ -2,9 +2,9 @@
 
 bokeh serve $1
 
+# bokeh serve --address 0.0.0.0 --host 192.168.0.143:5006 (client machine address)
+
 # see http://nbviewer.jupyter.org/github/bokeh/bokeh-notebooks/blob/master/tutorial/A1%20-%20Building%20gapminder.ipynb
 # see https://anaconda.org/bokeh/gapminder/notebook
 
 echo Specify the command line like ./bokeh-serve.sh bokeh-example-server.py
-echo need to tunnel localhost to the bokeh server
-echo Open the browser to http://localhost:5006/bokeh-example-server
