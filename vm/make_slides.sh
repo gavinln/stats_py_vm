695774b5e3c304cd7d6f3d5da5a36e317ce423ce
Gavin Noronha <gavinln@hotmail.com>
2017-10-27 16:39:09 -0700

    updated slide show

--- vm/make_slides.sh
+++ vm/make_slides.sh
@@ -3,9 +3,10 @@
 ROOT_DIR=/vagrant
 #export PYTHONPATH=$ROOT_DIR/python
 
-IPYTHON_CMD="ipython nbconvert"
+IPYTHON_CMD="jupyter nbconvert"
 BASIC_NOTEBOOKS=$ROOT_DIR/notebooks
 
-#$IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_database.ipynb --to slides
-#$IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_pyodbc.ipynb --to slides
-$IPYTHON_CMD $BASIC_NOTEBOOKS/Responsive_design.ipynb --to slides
+# $IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_database.ipynb --to slides
+# $IPYTHON_CMD $BASIC_NOTEBOOKS/SQL_pyodbc.ipynb --to slides
+# $IPYTHON_CMD $BASIC_NOTEBOOKS/Responsive_design.ipynb --to slides
+$IPYTHON_CMD $BASIC_NOTEBOOKS/titanic/titanic_seaborn.ipynb --to slides
